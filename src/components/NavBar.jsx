bb9f717cae932d2a9dfe1b6fa9fe93c5a282383d
Sebastian Enrique Gonzalez Benítez <71895275+gonzalezsebastian@users.noreply.github.com>
2024-01-30 13:53:52 -0500

    Responsive design

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,23 +1,59 @@
+import { useState, useEffect } from 'react';
 import Button from './Button';
-import '../styles/NavBar.css'
+import '../styles/NavBar.css';
+import { FaBars } from 'react-icons/fa';
 
 const NavBar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+    
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth > 992 && menuOpen) {
+                setMenuOpen(false);
+            }
+        };
+        
+        window.addEventListener('resize', handleResize);
+        
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, [menuOpen]);
+    
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
-        <nav className='navbar'>
-            <div className='logo'>
-                <div className="navbar-brand">SOPORTE</div>
-                <div className="navbar-brand-2">CARIBE</div>
+        <header>
+            <nav className='navbar'>
+                <div className='logo'>
+                    <div className="navbar-brand">SOPORTE</div>
+                    <div className="navbar-brand-2">CARIBE</div>
+                </div>
+                <div>
+                    <ul className="navbar-nav">
+                        <li className='nav-item'><a>¿Quiénes Sómos?</a></li>
+                        <li className='nav-item'><a>Clientes</a></li>
+                        <li className='nav-item'><a>Cotizaciones</a></li>
+                        <li className='nav-item'><a>Equipo</a></li>
+                    </ul>
+                </div>
+                <div className="botones">
+                    <Button title='Contáctanos'/>
+                </div>
+                <div className="menu-bars" onClick={toggleMenu}>
+                    <FaBars />
+                </div>
+            </nav>
+            <div className={menuOpen ? 'dropdown-menu open' : 'dropdown-menu'}>
+                <li className='nav-item'><a>¿Quiénes Sómos?</a></li>
+                <li className='nav-item'><a>Clientes</a></li>
+                <li className='nav-item'><a>Cotizaciones</a></li>
+                <li className='nav-item'><a>Equipo</a></li>
+                <li className='nav-item'><Button title='Contáctanos'/></li>
             </div>
-            <div>
-                <ul className="navbar-nav">
-                    <li className='nav-item'>¿Quiénes Sómos?</li>
-                    <li className='nav-item'>Clientes</li>
-                    <li className='nav-item'>Cotizaciones</li>
-                    <li className='nav-item'>Equipo</li>
-                </ul>
-            </div>
-            <Button title='Contáctanos'/>
-        </nav>
+        </header>
     );
 };
 
